e1b2edc36c112abce0a63c8d648c7c10f428fd9b
ekaterina <katerina_w@icloud.com>
2022-06-20 09:24:34 +0300

    add task2,4,6,8

---
+++ task2/Program.cs
@@ -0,0 +1,20 @@
+﻿/* Напишите программу, которая на вход
+принимает два числа и выдаёт, какое число большее, а
+какое меньшее.
+*/
+
+Console.WriteLine("Ведите первое число: ");
+int a = int.Parse(Console.ReadLine ());
+
+Console.WriteLine("Ведите второе число: ");
+int b = int.Parse(Console.ReadLine ());
+ 
+ if (a < b )
+{ 
+     Console.Write("Первое число меньше второго");
+}
+
+if ( a > b )
+{
+    Console.Write("Первое число больше второго");
+}
---
+++ task4/Program.cs
@@ -0,0 +1,16 @@
+﻿/*Задача 4: Напишите программу, которая принимает 
+на вход три числа и выдаёт максимальное из этих чисел.*/  
+ 
+Console.WriteLine("Ведите три числа: ");
+
+int a = int.Parse(Console.ReadLine ());
+int b = int.Parse(Console.ReadLine ());
+int c = int.Parse(Console.ReadLine ());
+int max = a;
+
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+
+Console.Write("max = ");
+Console.WriteLine(max);
